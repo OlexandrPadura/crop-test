c8018aa60926a1f78cada75c6bd11813b40259aa
Sasha Padura <olexandr.padura@eqblackbird.com>
2021-05-17 09:30:04 +0300

    [add] slider

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import React, {useState} from 'react';
-import {View} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {View, Dimensions, FlatList} from 'react-native';
 
 // NAME - react-native-photo-view-ex
 import PhotoView from 'react-native-photo-view-ex';
 import RNFetchBlob from 'rn-fetch-blob';
 
-export default ({}) => {
-  const url =
-    'https://banner2.cleanpng.com/20180713/tre/kisspng-react-native-web-application-javascript-google-scholar-logo-5b486117ca2a66.0620183815314701038281.jpg';
+const width = Dimensions.get('screen').width;
 
+const Img = ({item}) => {
   const [downloaded, setDownloaded] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -35,7 +34,7 @@ export default ({}) => {
     })
       .fetch(
         'GET',
-        'https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png',
+        'https://images.unsplash.com/photo-1621083439036-7a26badeab2e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1536&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxMjMxMjg0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=2048',
       )
       .then((resp) => {
         return resp.readFile('base64');
@@ -48,17 +47,19 @@ export default ({}) => {
         });
       });
   };
-  console.log(downloaded, 'downloaded');
+  const link = {};
+  if (downloaded) {
+    link.highSource = {uri: downloaded};
+  }
+
   return (
-    <View style={{width: '100%', height: '100%'}}>
+    <View style={{width: width, height: '100%'}} key={item.key}>
       <PhotoView
-        // key={downloaded}
-        source={{uri: downloaded ? downloaded : url}}
-        scale={1}
+        {...link}
+        source={{uri: item.img}}
         minimumZoomScale={1}
         maximumZoomScale={5}
         onScale={(e) => {
-          console.log(e.nativeEvent,'onScale');
           trigerCount();
           if (count > 20) {
             fetchFull();
@@ -70,6 +71,70 @@ export default ({}) => {
           height: '100%',
           position: 'absolute',
         }}
+      />
+    </View>
+  );
+};
+export default ({}) => {
+  const images = [
+    {
+      img:
+        'https://images.unsplash.com/photo-1621151580062-8c6634e43d39?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxMjMxMjEx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024',
+      key: 1,
+    },
+    {
+      img:
+        'https://images.unsplash.com/photo-1621151323071-f4265578a7e7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxMjMxMjMy&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024',
+      key: 2,
+    },
+    {
+      img:
+        'https://images.unsplash.com/photo-1621128136065-17a1375d46e0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxMjMxMjQy&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024',
+      key: 3,
+    },
+    {
+      img:
+        'https://images.unsplash.com/photo-1621151274364-67302cc6ace6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxMjMxMjUz&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024',
+      key: 4,
+    },
+    {
+      img:
+        'https://images.unsplash.com/photo-1621151528305-a620765e6345?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxMjMxMjY0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024',
+      key: 5,
+    },
+  ];
+
+  const renderItem = ({item}) => <Img {...{item}} />;
+
+  const flatListOptimizationProps = {
+    initialNumToRender: 0,
+    maxToRenderPerBatch: 1,
+    removeClippedSubviews: true,
+    scrollEventThrottle: 16,
+    windowSize: 5,
+    keyExtractor: useCallback((s) => String(s.key), []),
+    getItemLayout: useCallback(
+      (_, index) => ({
+        index,
+        length: width,
+        offset: index * width,
+      }),
+      [],
+    ),
+  };
+
+  return (
+    <View style={{width: '100%', height: '100%'}}>
+      <FlatList
+        data={images}
+        renderItem={renderItem}
+        pagingEnabled
+        horizontal
+        inverted
+        // canCancelContentTouches={false}
+        showsHorizontalScrollIndicator={false}
+        // onScroll={onScroll}
+        {...flatListOptimizationProps}
       />
     </View>
   );
